5cfbb31565a1c2e7818e0293137d696876de083b
sondrejk <sondrejk@gmail.com>
2026-06-16 09:18:51 +0200

    formatting

--- absorb.py
+++ absorb.py
@@ -4,6 +4,7 @@ import ansible_runner
 import time
 
 import signal
+
 print("SIGINT handler:", signal.getsignal(signal.SIGINT))
 signal.signal(signal.SIGINT, signal.SIG_DFL)
 
@@ -14,9 +15,10 @@ while True:
     frame, addr = s.recvfrom(2048)
     packet = scapy.all.Ether(frame)
     # print(packet.src)
-    
-    
-    if packet.src.startswith(("28:cd:c1", "b8:27:eb", "d8:3a:dd", "dc:a6:32", "e4:5f:01")):
+
+    if packet.src.startswith(
+        ("28:cd:c1", "b8:27:eb", "d8:3a:dd", "dc:a6:32", "e4:5f:01")
+    ):
         if packet.haslayer(scapy.all.DHCP):
             options = packet["DHCP"].options
             for option, value in options:
@@ -25,26 +27,22 @@ while True:
                     break
             else:
                 continue
-            time.sleep(1) # Give it a second
+            time.sleep(1)  # Give it a second
 
-            #Add RPI to inventory
-            #Run playbook with RPI IP as arg
+            # Add RPI to inventory
+            # Run playbook with RPI IP as arg
             args = {"new_host_ip": ip}
             print(f"PI detected at {packet.src}. Provisioning...")
             print("SIGINT handler:", signal.getsignal(signal.SIGINT))
             thread, res = ansible_runner.run_async(
                 private_data_dir=".",
-                playbook="provision-single.yml",
-                extravars=args
+                playbook="playbooks/provision-single.yml",
+                extravars=args,
             )
             thread.join()
             print("Provisioning complete")
             print(res.status)
             signal.signal(signal.SIGINT, signal.SIG_DFL)
             print("SIGINT handler:", signal.getsignal(signal.SIGINT))
-            
 
-            #RPI detected
-
-            
-
+            # RPI detected
