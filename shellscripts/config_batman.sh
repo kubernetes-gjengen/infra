8d48b341e4ee050b2ee63ad3d0c0f49d9040606e
sondrejk <sondrejk@gmail.com>
2026-07-03 12:32:33 +0200

    wip feat: dhcp

--- shellscripts/config_batman.sh
+++ shellscripts/config_batman.sh
@@ -16,7 +16,9 @@ fi
 # Stop network stuff
 sudo systemctl stop wpa_supplicant
 sudo rfkill unblock wifi
+sudo nmcli radio wifi on # persistent if you restart NM
 sudo ip link set wlan0 down
+sudo systemctl disable NetworkManager --now
 
 # Configure interface
 sudo iwconfig wlan0 mode ad-hoc
@@ -32,10 +34,14 @@ sudo batctl if add wlan0
 sudo ifconfig bat0 up
 
 sudo ip addr flush dev bat0
-sudo ip addr add $MESH_IP/24 dev bat0
+# sudo ip addr add $MESH_IP/24 dev bat0 #no
 sudo ip link set bat0 up
 
+#TODO: make this more clean. IPv4 auto on workers, static IP (gateway) on manager
+#Maybe split into separate scripts?
 if [ "$MESH_IP" != "$DEFAULT_GATEWAY" ]; then
-    sudo ip route add default via $DEFAULT_GATEWAY dev bat0
+#     sudo ip route add default via $DEFAULT_GATEWAY dev bat0 #begone
+else #manager only
+	sudo ip addr add $MESH_IP/24 dev bat0
 fi
 
