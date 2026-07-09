44c711e4764632468900a8c0eb8db8dfbc08688c
Ola Vanni Flaata <ola.flaata@gmail.com>
2026-07-09 17:17:06 +0200

    changed MTU of batman interface to avoid fragmentation

--- shellscripts/config_batman2_electric_boogaloo.sh
+++ shellscripts/config_batman2_electric_boogaloo.sh
@@ -6,3 +6,4 @@ sudo iwconfig wlan0 essid "meshnet"
 sudo iwconfig wlan0 channel 1 # or 6 or 11
 sudo ip link set wlan0 up
 sudo batctl if add wlan0
+sudo ip link set bat0 mtu 1476 # Make room for batman encapsulation 
